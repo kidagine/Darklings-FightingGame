0604df538e9966ae651bdd95afea06597dacd3b0
kidagine <davidkalatzis1999@hotmail.com>
2021-08-24 16:15:50 +0200

    Additions to stages and main menu.

---
+++ Assets/Scripts/UIScripts/BaseButton.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Audio))]
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Animator))]
+public class BaseButton : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler
+{
+    [SerializeField] public UnityEvent _onClickedAnimationEnd = default;
+    [SerializeField] private RectTransform _scrollView = default;
+    [SerializeField] private float _scrollAmount = default;
+    [SerializeField] private bool _ignoreFirstSelectSound = default;
+    protected Audio _audio;
+    protected Button _button;
+    protected Animator _animator;
+    private bool _isIgnoringFirstSelectSound;
+
+
+    void Awake()
+    {
+        _audio = GetComponent<Audio>();
+        _button = GetComponent<Button>();
+        _animator = GetComponent<Animator>();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (!_isIgnoringFirstSelectSound)
+        {
+            _audio.Sound("Selected").Play();
+        }
+        _animator.SetBool("IsSelected", true);
+        _isIgnoringFirstSelectSound = false;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        _animator.SetBool("IsSelected", false);
+    }
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+        _button.Select();
+    }
+
+	public virtual void OnPress()
+	{
+        _audio.Sound("Pressed").Play();
+        _animator.SetTrigger("Pressed");
+    }
+
+    public void OnClickedEndAnimationEvent()
+    {
+        _animator.Rebind();
+        _onClickedAnimationEnd?.Invoke();
+    }
+
+    public void MoveScrollDown(BaseEventData eventData)
+    {
+        AxisEventData axisEventData = eventData as AxisEventData;
+        if (axisEventData.moveDir == MoveDirection.Down)
+        {
+            _scrollView.anchoredPosition += new Vector2(0.0f, _scrollAmount);
+        }
+    }
+
+    public void MoveScrollUp(BaseEventData eventData)
+    {
+        AxisEventData axisEventData = eventData as AxisEventData;
+        if (axisEventData.moveDir == MoveDirection.Up)
+        {
+            _scrollView.anchoredPosition += new Vector2(0.0f, _scrollAmount);
+        }
+    }
+
+    void OnEnable()
+    {
+        _isIgnoringFirstSelectSound = _ignoreFirstSelectSound;
+    }
+
+    void OnDisable()
+    {
+        if (_ignoreFirstSelectSound)
+        {
+            _isIgnoringFirstSelectSound = true;
+        }
+        EventSystem.current.SetSelectedGameObject(null);
+        _animator.Rebind();
+    }
+}
---
+++ Assets/Scripts/UIScripts/BaseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseMenu : MonoBehaviour	
+{
+	[SerializeField] private Selectable _startingOption = default;
+
+
+	public void OpenMenuHideCurrent(BaseMenu menu)
+	{
+		gameObject.SetActive(false);
+		menu.Show();
+	}
+
+	public void OpenMenu(BaseMenu menu)
+	{
+		menu.gameObject.SetActive(true);
+	}
+
+	public void Show()
+	{
+		gameObject.SetActive(true);
+		StartCoroutine(ActivateCoroutine());
+	}
+
+	void OnEnable()
+	{
+		StartCoroutine(ActivateCoroutine());
+	}
+
+	IEnumerator ActivateCoroutine()
+	{
+		yield return null;
+		if (_startingOption != null)
+		{
+			_startingOption.Select();
+		}
+	}
+}
