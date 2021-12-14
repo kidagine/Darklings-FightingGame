18611f5f846e4b13e87e8e3fdd12f7497cefd360
kidagine <davidkalatzis1999@hotmail.com>
2021-12-14 01:45:09 +0100

    update.

---
+++ Assets/_Project/Tests/PlayMode/PerformancePlayTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Unity.PerformanceTesting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class PerformancePlayTest
+{
+    [UnityTest, Performance]
+    public IEnumerator Loading_GameScene()
+    {
+        using (Measure.Scope("Setup.LoadScene"))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        yield return null;
+
+        yield return Measure.Frames().Run();
+    }
+
+    [UnityTest, Performance]
+    public IEnumerator Measure_Total_AllocatedAndReservedMemory()
+    {
+        SceneManager.LoadScene("GameScene");
+        yield return null;
+
+        Player player = Object.FindObjectOfType<Player>();
+        PlayerController playerController = Object.FindObjectOfType<PlayerController>();
+        playerController.enabled = false;
+
+        Measure.Method(() =>
+        {
+            player.AttackAction();
+        })
+        .MeasurementCount(10)
+        .IterationsPerMeasurement(5)
+        .Run();
+    }
+}
