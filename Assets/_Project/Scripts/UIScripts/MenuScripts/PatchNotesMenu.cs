e88a7a40eb9a7680c95ca31b9fc7cb1b12cbe3dd
kidagine <davidkalatzis1999@hotmail.com>
2022-04-19 14:06:41 +0200

    Update for patchnotes

--- Assets/_Project/Scripts/UIScripts/MenuScripts/PatchNotesMenu.cs
+++ Assets/_Project/Scripts/UIScripts/MenuScripts/PatchNotesMenu.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PatchNotesMenu : BaseMenu
 {
 	[SerializeField] private BaseMenu[] _menues = default;
+	[SerializeField] private RectTransform _scrollView = default;
 	private int _previousMenuIndex = 0;
+	private int _scrollAmount = 5;
 
 
 	public void Back()
@@ -15,5 +17,22 @@ public class PatchNotesMenu : BaseMenu
 	public void SetPreviousMenuIndex(int index)
 	{
 		_previousMenuIndex = index;
+	}
+
+	void Update()
+	{
+		Scroll("KeyboardOne");
+		Scroll("KeyboardTwo");
+		Scroll("ControllerOne");
+		Scroll("ControllerTwo");
+	}
+
+	private void Scroll(string inputName)
+	{
+		float movement = Input.GetAxisRaw(inputName + "Vertical") * -1;
+		if (movement < 0 && _scrollView.anchoredPosition.y >= 0 || movement > 0 && _scrollView.anchoredPosition.y <= 700	)
+		{
+			_scrollView.anchoredPosition += new Vector2(0.0f, movement);
+		}
 	}
 }
