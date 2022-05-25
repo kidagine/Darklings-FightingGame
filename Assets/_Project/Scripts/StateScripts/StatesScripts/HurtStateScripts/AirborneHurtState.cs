278590888c82632338c68454af63d018b21990bf
kidagine <davidkalatzis1999@hotmail.com>
2022-05-25 21:17:11 +0200

    Minor fix

--- Assets/_Project/Scripts/StateScripts/StatesScripts/HurtStateScripts/AirborneHurtState.cs
+++ Assets/_Project/Scripts/StateScripts/StatesScripts/HurtStateScripts/AirborneHurtState.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class AirborneHurtState : HurtParentState
 {
 	[SerializeField] private GameObject _groundedPrefab = default;
 	private KnockdownState _knockdownState;
+	private Coroutine _canCheckGroundCoroutine;
+	private bool _canCheckGround;
 
-	void Awake()
+	protected override void Awake()
 	{
+		base.Awake();
 		_knockdownState = GetComponent<KnockdownState>();
 	}
 
@@ -17,6 +21,13 @@ public class AirborneHurtState : HurtParentState
 		_playerMovement.Knockback(new Vector2(
 			transform.root.localScale.x * -1.0f, _hurtAttack.knockbackDirection.y), _hurtAttack.knockback, _hurtAttack.knockbackDuration);
 		CameraShake.Instance.Shake(_hurtAttack.cameraShaker.intensity, _hurtAttack.cameraShaker.timer);
+		_canCheckGroundCoroutine = StartCoroutine(CanCheckGroundCoroutine());
+	}
+
+	IEnumerator CanCheckGroundCoroutine()
+	{
+		yield return new WaitForSeconds(0.1f);
+		_canCheckGround = true;
 	}
 
 	public override void UpdateLogic()
@@ -27,11 +38,21 @@ public class AirborneHurtState : HurtParentState
 
 	public void ToKnockdownState()
 	{
-		if (_playerMovement._isGrounded && _rigidbody.velocity.y <= 0.0f)
+		if (_playerMovement._isGrounded && _canCheckGround)
 		{
 			Instantiate(_groundedPrefab, transform.position, Quaternion.identity);
 			_audio.Sound("Landed").Play();
 			_stateMachine.ChangeState(_knockdownState);
+		}
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+		if (_canCheckGroundCoroutine != null)
+		{
+			_canCheckGround = false;
+			StopCoroutine(_canCheckGroundCoroutine);
 		}
 	}
 }
