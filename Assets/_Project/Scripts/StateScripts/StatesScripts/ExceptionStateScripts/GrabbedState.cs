3cb164721a81fdb12d2626bb79835e79efa8a65d
kidagine <davidkalatzis1999@hotmail.com>
2022-07-22 00:21:13 +0300

    Minor update

--- Assets/_Project/Scripts/StateScripts/StatesScripts/ExceptionStateScripts/GrabbedState.cs
+++ Assets/_Project/Scripts/StateScripts/StatesScripts/ExceptionStateScripts/GrabbedState.cs
@@ -1,12 +1,16 @@
+using UnityEngine;
+
 public class GrabbedState : State
 {
 	private DeathState _deathState;
 	private KnockdownState _knockdownState;
+	private AirborneHurtState _airborneHurtState;
 
 	private void Awake()
 	{
 		_deathState = GetComponent<DeathState>();
 		_knockdownState = GetComponent<KnockdownState>();
+		_airborneHurtState = GetComponent<AirborneHurtState>();
 	}
 
 	public override void Enter()
@@ -33,6 +37,14 @@ public class GrabbedState : State
 			ToDeathState();
 		}
 		_stateMachine.ChangeState(_knockdownState);
+		return true;
+	}
+
+	public override bool ToAirborneHurtState(AttackSO attack)
+	{
+		Debug.Log("eex");
+		_airborneHurtState.Initialize(attack);
+		_stateMachine.ChangeState(_airborneHurtState);
 		return true;
 	}
 
