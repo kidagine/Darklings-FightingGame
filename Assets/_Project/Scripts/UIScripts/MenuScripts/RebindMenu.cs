604954f5879d5f3217b99eee8ed58bfa0dad3847
kidagine <davidkalatzis1999@hotmail.com>
2022-08-13 16:58:53 +0300

    rebind update

--- Assets/_Project/Scripts/UIScripts/MenuScripts/RebindMenu.cs
+++ Assets/_Project/Scripts/UIScripts/MenuScripts/RebindMenu.cs
@@ -1,14 +1,19 @@
 using Demonics.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputActionRebindingExtensions;
 
 public class RebindMenu : BaseMenu
 {
+	[SerializeField] private PlayerInput _playerInput = default;
 	[SerializeField] private Button _firstCharacterButton = default;
 	[SerializeField] private CharacterAssistSelector _characterAssistSelector = default;
 	[SerializeField] private CharacterColorSelector _characterColorSelector = default;
-	[SerializeField] private GameObject _assignButtonImage = default;		
+	[SerializeField] private GameObject _assignButtonImage = default;
+	private InputAction _inputAction = default;
+	private RebindingOperation _rebindingOperation;
 
 	public void HideRebind()
 	{
@@ -19,14 +24,25 @@ public class RebindMenu : BaseMenu
 		}
 	}
 
-	public void AssignButton()
+	public void AssignButton(InputActionReference actionReference)
 	{
+		_inputAction = _playerInput.actions.FindAction(actionReference.action.id);
 		EventSystem.current.sendNavigationEvents = false;
+		Debug.Log(_inputAction.GetBindingDisplayString(0, InputBinding.DisplayStringOptions.DontUseShortDisplayNames));
 		_assignButtonImage.SetActive(true);
+		_rebindingOperation = _inputAction.PerformInteractiveRebinding().OnComplete(operation => RebindComplete());
+		_rebindingOperation.Start();
+	}
+
+	private void RebindComplete()
+	{
+		EventSystem.current.sendNavigationEvents = true;
+		_assignButtonImage.SetActive(false);
+		_rebindingOperation.Dispose();
 	}
 
 	public void ResetRebindToDefault()
 	{
-
+		InputActionRebindingExtensions.RemoveAllBindingOverrides(_playerInput.currentActionMap);
 	}
 }
