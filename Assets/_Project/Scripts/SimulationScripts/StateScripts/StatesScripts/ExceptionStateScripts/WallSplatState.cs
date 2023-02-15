c94d6b30ce1e25d97e7bf65993018c43b88a9974
David Kalatzis <Davidkalatzis@gmail.com>
2023-02-15 17:58:52 +0200

    Minor change for wallsplat & fixing training positions

--- Assets/_Project/Scripts/SimulationScripts/StateScripts/StatesScripts/ExceptionStateScripts/WallSplatState.cs
+++ Assets/_Project/Scripts/SimulationScripts/StateScripts/StatesScripts/ExceptionStateScripts/WallSplatState.cs
@@ -21,6 +21,8 @@ public class WallSplatState : State
             player.sound = "WallSplat";
             player.enter = true;
             player.animationFrames = 0;
+            player.attackHurtNetwork.knockbackArc = 30;
+            player.attackHurtNetwork.knockbackDuration = 24;
         }
         player.velocity = DemonicsVector2.Zero;
         player.hurtbox.active = false;
