1074a993066a7f84ddf4df567be98e4182d79d4e
David Kalatzis <Davidkalatzis@gmail.com>
2023-03-22 00:43:01 +0200

    Set up new stage Ruins

--- Assets/_Project/Scripts/EnumScripts/StageTypes.cs
+++ Assets/_Project/Scripts/EnumScripts/StageTypes.cs
@@ -2,11 +2,11 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public enum StageTypeEnum { Grid, TheVoid, Graveyard, Arena, Bridge, Temple };
+public enum StageTypeEnum { Grid, TheVoid, Graveyard, Arena, Bridge, Temple, Ruins };
 
 public class StageType : MonoBehaviour
 {
-	[SerializeField] private StageTypeEnum _stageTypeEnum = default;
+    [SerializeField] private StageTypeEnum _stageTypeEnum = default;
 
-	public StageTypeEnum StageTypeEnum { get { return _stageTypeEnum; } private set { } }
+    public StageTypeEnum StageTypeEnum { get { return _stageTypeEnum; } private set { } }
 }
