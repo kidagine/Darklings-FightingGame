c1986882e0d9fced1a7c8a63425a38639de7fa09
David Kalatzis <Davidkalatzis@gmail.com>
2023-04-23 17:39:12 +0300

    Add attack buffer when jumping (because of pre jump)

--- Assets/_Project/Scripts/SimulationScripts/StateScripts/StatesScripts/AirStateScripts/JumpForwardState.cs
+++ Assets/_Project/Scripts/SimulationScripts/StateScripts/StatesScripts/AirStateScripts/JumpForwardState.cs
@@ -13,6 +13,28 @@ public class JumpForwardState : AirParentState
             player.hasJumped = true;
             player.animationFrames = 0;
             player.velocity = new DemonicsVector2(2 * (DemonicsFloat)player.jumpDirection, player.playerStats.JumpForce);
+            if (player.inputBuffer.CurrentInput().frame != 0)
+            {
+                player.isAir = true;
+                player.inputBuffer.inputItems[player.inputBuffer.index].frame = 0;
+                if (player.inputBuffer.CurrentInput().inputEnum == InputEnum.Special)
+                {
+                    Arcana(player, player.isAir);
+                }
+                else
+                {
+                    if (!(player.attackInput == InputEnum.Medium && player.isCrouch))
+                    {
+                        if (player.inputBuffer.CurrentInput().inputEnum != InputEnum.Throw)
+                        {
+                            if (!(player.attackInput == InputEnum.Heavy && !player.isCrouch && player.inputBuffer.CurrentInput().inputEnum == InputEnum.Heavy && player.direction.y >= 0))
+                            {
+                                Attack(player, player.isAir);
+                            }
+                        }
+                    }
+                }
+            }
             return;
         }
         player.animationFrames++;
